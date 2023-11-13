143948f40c782925c2893e057b8c28a5f14a6358
Paul_Botchwey <botchweypaul0001@gmail.com>
2023-11-13 00:48:40 +0000

    Scan once

--- lib/pages/verify_tenants.dart
+++ lib/pages/verify_tenants.dart
@@ -1,140 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
-
-// class VerifyTenantsPage extends StatefulWidget {
-//   final String accessToken;
-
-//   VerifyTenantsPage({required this.accessToken});
-
-//   @override
-//   _VerifyTenantsPageState createState() => _VerifyTenantsPageState();
-// }
-
-// class _VerifyTenantsPageState extends State<VerifyTenantsPage> {
-//   bool isLoading = false;
-//   String scanMessage = "Scan the QR code to verify the tenant.";
-
-//   Future<void> sendHardcodedVerificationCode() async {
-//     setState(() {
-//       isLoading = true;
-//       scanMessage = "Verifying...";
-//     });
-
-//     try {
-//       final response = await http.post(
-//         Uri.parse(
-//             'https://ethenatx.pythonanywhere.com/management/verify-tenant/'),
-//         headers: {
-//           'Authorization': 'Bearer ${widget.accessToken}',
-//           'Content-Type': 'application/json; charset=UTF-8',
-//         },
-//         body: jsonEncode({
-//           'verification_code':
-//               "EVAdc0a-05r0-En-011b0-lem-82dc4dbf-4fd5-42e9-8f4b-41188c18234f-enoch-20951781-072024k0-lemon-15:34:33.411574-Grj",
-//         }),
-//       );
-
-//       if (response.statusCode == 200) {
-//         final responseData = json.decode(response.body);
-//         if (responseData.containsKey('tenant_name') &&
-//             responseData.containsKey('student_id')) {
-//           print("it coming:");
-//           print(responseData["message"]);
-//           print(responseData);
-//           print(response.statusCode);
-//           print(response.body);
-//           // Display the student's information
-//           setState(() {
-//             isLoading = false;
-//             scanMessage = "Scan the QR code to verify the tenant.";
-//           });
-
-//           showDialog(
-//             context: context,
-//             builder: (context) {
-//               return AlertDialog(
-//                 title: Text('Verification Successful'),
-//                 content: Column(
-//                   mainAxisSize: MainAxisSize.min,
-//                   children: [
-//                     Text('Student: ${responseData['hostel_name']}'),
-//                     Text('Room Number: ${responseData['room_number']}'),
-//                     // Text('Hostel: ${responseData['Hostel']}'),
-//                     // Text('Verification Status: ${responseData['Verified']}'),
-//                   ],
-//                 ),
-//                 actions: [
-//                   TextButton(
-//                     onPressed: () {
-//                       Navigator.pop(context);
-//                     },
-//                     child: Text('OK'),
-//                   ),
-//                 ],
-//               );
-//             },
-//           );
-//         } else {
-//           print("Is it coming:");
-//           print(responseData["message"]);
-//           print(responseData);
-//           print(response.statusCode);
-//           print(response.body);
-//           // Tenant is not verified or has not paid
-//           setState(() {
-//             isLoading = false;
-//             scanMessage = "Tenant is not verified or has not paid.";
-//           });
-//         }
-//       } else {
-//         final responseData = json.decode(response.body);
-
-//         print("An error occurred:");
-//         print(responseData["message"]);
-//         print(responseData);
-//         print(response.statusCode);
-//         print(response.body);
-//         // Handle other response codes or errors
-//         setState(() {
-//           isLoading = false;
-//           scanMessage = "An error occurred. Please try again later.";
-//         });
-//       }
-//     } catch (e) {
-//       // Handle network or other errors
-//       print('Error: $e');
-//       setState(() {
-//         isLoading = false;
-//         scanMessage = "A Network error occurred. Please try again later.";
-//       });
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Verify Tenants'),
-//       ),
-//       body: Column(
-//         children: <Widget>[
-//           SizedBox(height: 10),
-//           Text(
-//             scanMessage,
-//             style: TextStyle(fontSize: 18),
-//           ),
-//           if (isLoading) CircularProgressIndicator(),
-//           ElevatedButton(
-//             onPressed: sendHardcodedVerificationCode,
-//             child: Text('Verify Hardcoded Code'),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 // ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
@@ -154,7 +17,8 @@ class VerifyTenantsPage extends StatefulWidget {
 class _VerifyTenantsPageState extends State<VerifyTenantsPage> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   QRViewController? controller;
-  String? result = "";
+  String? result;
+  bool isVerificationDialogShown = false;
 
   @override
   void reassemble() {
@@ -224,21 +88,17 @@ class _VerifyTenantsPageState extends State<VerifyTenantsPage> {
             child: QRView(
               key: qrKey,
               onQRViewCreated: _onQRViewCreated,
-              // overlay: QrScannerOverlayShape(
-              //   borderWidth: 20,
-              //   borderLength: 40,
-              //   borderRadius: 20,
-              //   cutOutSize: MediaQuery.of(context).size.width,
-              // ),
             ),
           ),
           Expanded(
             flex: 1,
             child: Center(
-              child: Text(
-                'Result: \n $result',
-                style: TextStyle(fontSize: 16),
-              ),
+              child: isVerificationDialogShown
+                  ? CircularProgressIndicator()
+                  : Text(
+                      'Result: \n $result',
+                      style: TextStyle(fontSize: 16),
+                    ),
             ),
           ),
         ],
@@ -255,10 +115,13 @@ class _VerifyTenantsPageState extends State<VerifyTenantsPage> {
   void _onQRViewCreated(QRViewController controller) {
     this.controller = controller;
     controller.scannedDataStream.listen((scanData) {
-      setState(() {
-        result = scanData.code;
-      });
-      verifyTenant(scanData.code);
+      if (!isVerificationDialogShown) {
+        setState(() {
+          result = scanData.code;
+          isVerificationDialogShown = true;
+        });
+        verifyTenant(scanData.code);
+      }
     });
   }
 
@@ -281,8 +144,6 @@ class _VerifyTenantsPageState extends State<VerifyTenantsPage> {
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
-      // if (data['Verified'] == 'Verified') {
-      // Tenant is verified
       showDialog(
         context: context,
         builder: (context) {
@@ -301,6 +162,9 @@ class _VerifyTenantsPageState extends State<VerifyTenantsPage> {
             actions: [
               TextButton(
                 onPressed: () {
+                  setState(() {
+                    isVerificationDialogShown = false;
+                  });
                   Navigator.of(context).pop();
                 },
                 child: Text('Close'),
@@ -309,27 +173,6 @@ class _VerifyTenantsPageState extends State<VerifyTenantsPage> {
           );
         },
       );
-      // }
-      //  else {
-      //   // Tenant is not verified
-      //   showDialog(
-      //     context: context,
-      //     builder: (context) {
-      //       return AlertDialog(
-      //         title: Text('Verification Failed'),
-      //         content: Text('Tenant is not verified or has not paid.'),
-      //         actions: [
-      //           TextButton(
-      //             onPressed: () {
-      //               Navigator.of(context).pop();
-      //             },
-      //             child: Text('Close'),
-      //           ),
-      //         ],
-      //       );
-      //     },
-      //   );
-      // }
     } else {
       // Handle errors
       showDialog(
@@ -341,6 +184,9 @@ class _VerifyTenantsPageState extends State<VerifyTenantsPage> {
             actions: [
               TextButton(
                 onPressed: () {
+                  setState(() {
+                    isVerificationDialogShown = false;
+                  });
                   Navigator.of(context).pop();
                 },
                 child: Text('Close'),
