30b7bf104b09b7b958ca7e540d9c5f75ae29b1e3
Paul_Botchwey <botchweypaul0001@gmail.com>
2023-12-19 16:51:34 +0000

    added the notification messages

--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -1,210 +1,7 @@
-// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-// import 'dart:convert';
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-
-// class EditProfilePage extends StatefulWidget {
-//   final String accessToken;
-
-//   const EditProfilePage({Key? key, required this.accessToken})
-//       : super(key: key);
-
-//   @override
-//   _EditProfilePageState createState() => _EditProfilePageState();
-// }
-
-// class _EditProfilePageState extends State<EditProfilePage> {
-//   TextEditingController hostelNameController = TextEditingController();
-//   TextEditingController managerContactController = TextEditingController();
-//   TextEditingController hostelContactController = TextEditingController();
-//   TextEditingController mobileMoneyController = TextEditingController();
-//   TextEditingController startPriceController = TextEditingController();
-//   TextEditingController endPriceController = TextEditingController();
-//   TextEditingController mainWebsiteController = TextEditingController();
-//   TextEditingController locationController = TextEditingController();
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     // Fetch data from API and populate the fields
-//     fetchAndPopulateData();
-//   }
-
-//   Future<void> fetchAndPopulateData() async {
-//     final url =
-//         'https://ethenatx.pythonanywhere.com/management/management-profile/';
-
-//     try {
-//       final response = await http.get(
-//         Uri.parse(url),
-//         headers: {'Authorization': 'Bearer ${widget.accessToken}'},
-//       );
-
-//       if (response.statusCode == 200) {
-//         final data = json.decode(response.body);
-//         // Get the price range
-//         String priceRange = data['price_range'];
-
-//         // Split the price range
-//         List<String> prices = priceRange.split('-');
-
-//         setState(() {
-//           hostelNameController.text = data['hostel_name'] ?? '';
-//           managerContactController.text = data['manager_contact'] ?? '';
-//           hostelContactController.text = data['hostel_contact'] ?? '';
-//           mobileMoneyController.text = data['mobile_money'] ?? '';
-//           // Set the start and end price controllers
-//           startPriceController.text = prices[0];
-//           endPriceController.text = prices[1];
-//           mainWebsiteController.text = data['main_website'];
-//           locationController.text = data['location'] ?? '';
-//         });
-//       } else {
-//         throw Exception('Failed to load data');
-//       }
-//     } catch (e) {
-//       print('Error fetching data: $e');
-//       // Handle the error appropriately
-//     }
-//   }
-
-//   Future<void> updateProfile() async {
-//     String priceRange =
-//         '${startPriceController.text}-${endPriceController.text}';
-//     final url =
-//         'https://ethenatx.pythonanywhere.com/management/management-profile/';
-//     try {
-//       final response = await http.put(
-//         Uri.parse(url),
-//         headers: {
-//           'Authorization': 'Bearer ${widget.accessToken}',
-//           'Content-Type': 'application/json',
-//         },
-//         body: json.encode({
-//           "hostel_name": hostelNameController.text,
-//           "manager_contact": managerContactController.text,
-//           "hostel_contact": hostelContactController.text,
-//           "mobile_money": mobileMoneyController.text,
-//           "price_range": priceRange,
-//           "main_website": mainWebsiteController.text,
-//           "location": locationController.text,
-//         }),
-//       );
-
-//       if (response.statusCode == 200) {
-//         // Profile updated successfully
-//         // You might want to navigate back or show a success message
-//       } else {
-//         throw Exception('Failed to update profile');
-//       }
-//     } catch (e) {
-//       print('Error updating profile: $e');
-//       // Handle the error appropriately
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         elevation: 0,
-//         backgroundColor: Colors.white,
-//         leading: IconButton(
-//           icon: const Icon(Icons.arrow_back, color: Color(0xFFF59B15)),
-//           onPressed: () {
-//             Navigator.of(context).pop();
-//           },
-//         ),
-//         title: const Text(
-//           'Edit Profile',
-//           style: TextStyle(
-//             fontWeight: FontWeight.w600,
-//             color: Color(0xFFF59B15),
-//             fontSize: 25,
-//           ),
-//         ),
-//       ),
-//       body: Container(
-//         height: double.infinity,
-//         child: Column(
-//           children: [
-//             Expanded(
-//               child: SingleChildScrollView(
-//                 padding: const EdgeInsets.all(16),
-//                 child: Column(
-//                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                   children: [
-//                     _buildTextField('Hostel Name', hostelNameController),
-//                     _buildTextField(
-//                         'Manager Contact', managerContactController),
-//                     _buildTextField('Hostel Contact', hostelContactController),
-//                     _buildTextField('Mobile Money', mobileMoneyController),
-//                     _buildPriceRangeFields(),
-//                     _buildTextField('Main Website', mainWebsiteController),
-//                     _buildTextField('Location', locationController),
-//                   ],
-//                 ),
-//               ),
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.all(8.0),
-//               child: SizedBox(
-//                 width: double.infinity,
-//                 height: 35,
-//                 child: ElevatedButton(
-//                   onPressed: updateProfile,
-//                   child: Row(
-//                     mainAxisSize: MainAxisSize.min,
-//                     children: [
-//                       Text('Update Profile', style: TextStyle(fontSize: 16)),
-//                       SizedBox(width: 8),
-//                       FaIcon(FontAwesomeIcons.solidFloppyDisk, size: 18),
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-//   Widget _buildTextField(String label, TextEditingController controller) {
-//     return Padding(
-//       padding: const EdgeInsets.only(bottom: 16),
-//       child: TextField(
-//         controller: controller,
-//         decoration: InputDecoration(
-//           labelText: label,
-//         ),
-//       ),
-//     );
-//   }
-
-//   Widget _buildPriceRangeFields() {
-//     return Padding(
-//       padding: const EdgeInsets.only(bottom: 16),
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//         children: [
-//           Expanded(
-//             child: _buildTextField('Start Price', startPriceController),
-//           ),
-//           SizedBox(width: 16),
-//           Expanded(
-//             child: _buildTextField('End Price', endPriceController),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class EditProfilePage extends StatefulWidget {
   final String accessToken;
@@ -217,17 +14,23 @@ class EditProfilePage extends StatefulWidget {
 }
 
 class _EditProfilePageState extends State<EditProfilePage> {
-  // Add these variables to store the image URLs
-  String? _profilePictureUrl;
-  String? _hostelImageUrl;
+  TextEditingController hostelNameController = TextEditingController();
+  TextEditingController managerContactController = TextEditingController();
+  TextEditingController hostelContactController = TextEditingController();
+  TextEditingController mobileMoneyController = TextEditingController();
+  TextEditingController startPriceController = TextEditingController();
+  TextEditingController endPriceController = TextEditingController();
+  TextEditingController mainWebsiteController = TextEditingController();
+  TextEditingController locationController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    fetchProfileData();
+    // Fetch data from API and populate the fields
+    fetchAndPopulateData();
   }
 
-  Future<void> fetchProfileData() async {
+  Future<void> fetchAndPopulateData() async {
     final url =
         'https://ethenatx.pythonanywhere.com/management/management-profile/';
 
@@ -236,16 +39,26 @@ class _EditProfilePageState extends State<EditProfilePage> {
         Uri.parse(url),
         headers: {'Authorization': 'Bearer ${widget.accessToken}'},
       );
-      print(response.statusCode);
+
       if (response.statusCode == 200) {
+        print('Get response ${response.statusCode}');
         final data = json.decode(response.body);
-        print('Rsponse body: ${response.body}');
+        // Get the price range
+        String priceRange = data['price_range'];
+
+        // Split the price range
+        List<String> prices = priceRange.split('-');
 
         setState(() {
-          const baseUrl = 'https://ethenatx.pythonanywhere.com';
-          _profilePictureUrl =
-              '$baseUrl${data['hostel_manager_profile_picture']}';
-          _hostelImageUrl = '$baseUrl${data['hostel_image']}';
+          hostelNameController.text = data['hostel_name'] ?? '';
+          managerContactController.text = data['manager_contact'] ?? '';
+          hostelContactController.text = data['hostel_contact'] ?? '';
+          mobileMoneyController.text = data['mobile_money'] ?? '';
+          // Set the start and end price controllers
+          startPriceController.text = prices[0];
+          endPriceController.text = prices[1];
+          mainWebsiteController.text = data['main_website'] ?? '';
+          locationController.text = data['location'] ?? '';
         });
       } else {
         throw Exception('Failed to load data');
@@ -256,33 +69,164 @@ class _EditProfilePageState extends State<EditProfilePage> {
     }
   }
 
+  Future<void> updateProfile() async {
+    String priceRange =
+        '${startPriceController.text}-${endPriceController.text}';
+    final url =
+        'https://ethenatx.pythonanywhere.com/management/management-profile/';
+    try {
+      final response = await http.put(
+        Uri.parse(url),
+        headers: {
+          'Authorization': 'Bearer ${widget.accessToken}',
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({
+          "hostel_name": hostelNameController.text,
+          "manager_contact": managerContactController.text,
+          "hostel_contact": hostelContactController.text,
+          "mobile_money": mobileMoneyController.text,
+          "price_range": priceRange,
+          "main_website": mainWebsiteController.text,
+          "location": locationController.text,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        print('Put response ${response.statusCode}');
+        _showSuccessSnackbar('Profile updated successfully');
+      } else {
+        throw Exception('Failed to update profile');
+      }
+    } catch (e) {
+      print('Error updating profile: $e');
+      _showErrorSnackbar(e.toString());
+    }
+  }
+
+  void _showErrorSnackbar(String errorMessage) {
+    // Extracting error message from the response
+    final startBracket = errorMessage.indexOf('[');
+    final endBracket = errorMessage.indexOf(']');
+    if (startBracket != -1 && endBracket != -1) {
+      errorMessage = errorMessage.substring(startBracket + 1, endBracket);
+    }
+
+    // Display a different message for "Enter a valid URL" error
+    if (errorMessage == 'Enter a valid URL.') {
+      errorMessage = 'Invalid website URL';
+    }
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(errorMessage),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+
+  void _showSuccessSnackbar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Edit Profile'),
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Color(0xFFF59B15)),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: const Text(
+          'Edit Profile',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            color: Color(0xFFF59B15),
+            fontSize: 25,
+          ),
+        ),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
+      body: Container(
+        height: double.infinity,
         child: Column(
           children: [
-            // Add widgets to display the images
-            _profilePictureUrl != null
-                ? CircleAvatar(
-                    radius: 50,
-                    backgroundImage: NetworkImage(_profilePictureUrl!),
-                  )
-                : CircularProgressIndicator(),
-            SizedBox(height: 16),
-            _hostelImageUrl != null
-                ? Image.network(
-                    _hostelImageUrl!,
-                    height: 200,
-                    fit: BoxFit.cover,
-                  )
-                : CircularProgressIndicator(),
+            Expanded(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildTextField('Hostel Name', hostelNameController),
+                    _buildTextField(
+                        'Manager Contact', managerContactController),
+                    _buildTextField('Hostel Contact', hostelContactController),
+                    _buildTextField('Mobile Money', mobileMoneyController),
+                    _buildPriceRangeFields(),
+                    _buildTextField('Main Website', mainWebsiteController),
+                    _buildTextField('Location', locationController),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 35,
+                child: ElevatedButton(
+                  onPressed: updateProfile,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text('Update Profile', style: TextStyle(fontSize: 16)),
+                      SizedBox(width: 8),
+                      FaIcon(FontAwesomeIcons.solidFloppyDisk, size: 18),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(String label, TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPriceRangeFields() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: _buildTextField('Start Price', startPriceController),
+          ),
+          SizedBox(width: 16),
+          Expanded(
+            child: _buildTextField('End Price', endPriceController),
+          ),
+        ],
       ),
     );
   }
